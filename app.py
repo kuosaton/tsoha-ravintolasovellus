c9f31b22304fb664172f372ff546968194d6603b
Tony Kuosa <tony.kuosa@outlook.com>
2024-05-30 16:13:06 +0300

    Added text() for sql code

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ db = SQLAlchemy(app)
 def index():
     db.session.execute(text("INSERT INTO visitors (time) VALUES (NOW())"))
     db.session.commit()
-    result = db.session.execute(("SELECT COUNT(*) FROM visitors"))
+    result = db.session.execute(text("SELECT COUNT(*) FROM visitors"))
     counter = result.fetchone()[0]
     return render_template("index.html", counter=counter) 
